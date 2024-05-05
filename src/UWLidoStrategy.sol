4b319dc9f65a087c4b99f13528410fea4d8d8e6e
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-05-06 00:33:47 +0200

    refactor: IUWDeposit for LIDO

--- src/UWLidoStrategy.sol
+++ src/UWLidoStrategy.sol
@@ -1,47 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import { IUWDeposit } from "./interfaces/IUWDeposit.sol";
-import { UWBaseStrategy } from "./abstract/UWBaseStrategy.sol";
-import { UWConstants} from "./libraries/UWConstants.sol";
-import { IUWErrors } from "./interfaces/IUWErrors.sol";
-interface ILido {
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {IUWDeposit} from "./interfaces/IUWDeposit.sol";
+import {UWBaseStrategy} from "./abstract/UWBaseStrategy.sol";
+import {UWConstants} from "./libraries/UWConstants.sol";
+import {IUWErrors} from "./interfaces/IUWErrors.sol";
+
+interface ILido is IERC20 {
     function submit(address _referral) external payable returns (uint256);
 }
 
-interface IwstETH {
-    function wrap(uint256 _stETHAmount) external returns (uint256); 
+interface IwstETH is IERC20 {
+    function wrap(uint256 _stETHAmount) external returns (uint256);
     function unwrap(uint256 _wstETHAmount) external returns (uint256);
 }
 
 contract UWLidoStrategy is IUWDeposit, UWBaseStrategy {
-    ILido immutable LIDO;
-    address immutable wstETH;
-    address immutable referral;
+    ILido internal immutable LIDO;
+    IwstETH internal immutable wstETH;
+    address internal immutable referral;
 
-    constructor(ILido _lido, address _wstETH, address _referral) {
-       LIDO = _lido; 
-       wstETH = _wstETH;
-       referral = _referral;
+    constructor(ILido _lido, IwstETH _wstETH, address _referral) {
+        LIDO = _lido;
+        wstETH = _wstETH;
+        referral = _referral;
     }
 
+    /// @notice Deposit Ether into LIDO.
+    /// @param position choose which asset to receive stETH or wstETH.
+    /// @param asset UWConstants.NATIVE_ASSET is the only asset supported.
+    /// @param amount the amount of ether to deposit.
     function deposit(bytes32 position, address asset, uint256 amount) external payable override {
         // Only native Ether is supported.
-        if(asset != UWConstants.NATIVE_ASSET) revert IUWErrors.UNSUPPORTED_ASSET(asset);
-        // Check that the position is one of the two available options.  
-        if(address(uint160(uint256(position))) != address(LIDO) && address(uint160(uint256(position))) != wstETH)
+        if (asset != UWConstants.NATIVE_ASSET) revert IUWErrors.UNSUPPORTED_ASSET(asset);
+        // Check that the position is one of the two available options.
+        if (
+            address(uint160(uint256(position))) != address(LIDO)
+                && address(uint160(uint256(position))) != address(wstETH)
+        ) {
             revert IUWErrors.INVALID_POSITION(position);
-        
-        // Check that the amount to deposit is within the allowed bound of Lido and that the user has enough balance 
-        if(amount < 100 wei || amount > address(this).balance || amount > 1000 ether)
+        }
+
+        // Check that the amount to deposit is within the allowed bound of Lido and that the user has enough balance
+        if (amount < 100 wei || amount > address(this).balance || amount > 1000 ether) {
             revert IUWErrors.ASSET_AMOUNT_OUT_OF_BOUNDS(
-                asset,
-                100 wei,
-                address(this).balance < 1000 ether ? address(this).balance : 1000 ether,
-                amount
+                asset, 100 wei, address(this).balance < 1000 ether ? address(this).balance : 1000 ether, amount
             );
-       
+        }
+
+        // Perform the ETH -> stETH deposit
         uint256 _receivedAmount = LIDO.submit{value: amount}(referral);
-       
+
+        // If the position was for wstETH, then we perform the stETH -> wstETH
+        if (address(uint160(uint256(position))) == address(wstETH)) {
+            LIDO.approve(address(wstETH), _receivedAmount);
+            wstETH.wrap(_receivedAmount);
+        }
     }
 }
--- test/LidoStrategy.t.sol
+++ test/LidoStrategy.t.sol
@@ -1,19 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import {ILido, UWLidoStrategy} from "../src/UWLidoStrategy.sol";
+import "../src/UWLidoStrategy.sol";
 
 import {Test} from "forge-std/Test.sol";
 
 contract LidoStrategyTest is Test {
-    ILido LIDO;
-    address wstETH;
+    ILido LIDO = ILido(address(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84));
+    IwstETH wstETH = IwstETH(address(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0));
     address referral;
     UWLidoStrategy strategy;
 
     function setUp() public {
+        // Configure forking mainnet
+        vm.createSelectFork("https://rpc.payload.de");
+
+        vm.label(address(LIDO), "LIDO");
+        vm.label(address(wstETH), "wstETH");
         strategy = new UWLidoStrategy(LIDO, wstETH, referral);
     }
 
-    function testNothing() public {}
+    function testDeposit() public {
+        vm.deal(address(strategy), 10 ether);
+        strategy.deposit(bytes32(uint256(uint160(address(LIDO)))), UWConstants.NATIVE_ASSET, 9 ether);
+    }
+
+    function testDepositForWstETH() public {
+        vm.deal(address(strategy), 10 ether);
+        strategy.deposit(bytes32(uint256(uint160(address(wstETH)))), UWConstants.NATIVE_ASSET, 9 ether);
+    }
+
+    function testDeposit_amountBelowBound_reverts() public {
+        vm.deal(address(strategy), 10 ether);
+
+        // TODO: vm.expectRevert(IUWErrors.ASSET_AMOUNT_OUT_OF_BOUNDS.selector);
+        vm.expectRevert();
+        strategy.deposit(bytes32(uint256(uint160(address(wstETH)))), UWConstants.NATIVE_ASSET, 99 wei);
+    }
 }
