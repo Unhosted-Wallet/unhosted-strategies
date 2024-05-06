71cdb38ad6b0d1ef882f0058826cafad3644240b
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-05-07 00:00:58 +0200

    fix: tests

--- test/CompoundStrategy.t.sol
+++ test/CompoundStrategy.t.sol
@@ -7,7 +7,7 @@ import {Test} from "forge-std/Test.sol";
 
 contract CompoundStrategyTest is Test {
     IComet COMET = IComet(address(0xc3d688B66703497DAA19211EEdff47f25384cdc3));
-    wETH WETH = wETH(address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
+    WrappedETH WETH = WrappedETH(address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2));
     IERC20 USDC = IERC20(address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48));
     UWCompoundV3Strategy strategy;
 
