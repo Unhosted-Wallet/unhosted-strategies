5543eec13bf7790e978a50d28d9b1ae49543c2cf
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-05-16 23:32:04 +0200

    style: change solhint config, fix solhint errors, format lido strategy

--- src/UWLidoStrategy.sol
+++ src/UWLidoStrategy.sol
@@ -12,28 +12,37 @@ import {IUWErrors} from "./interfaces/IUWErrors.sol";
 
 interface ILido is IERC20 {
     function submit(address _referral) external payable returns (uint256);
-    function getPooledEthByShares(uint256 _sharesAmount) external view returns (uint256);
+    function getPooledEthByShares(
+        uint256 _sharesAmount
+    ) external view returns (uint256);
 }
 
 interface IwstETH is IERC20 {
     function wrap(uint256 _stETHAmount) external returns (uint256);
     function unwrap(uint256 _wstETHAmount) external returns (uint256);
-    function getStETHByWstETH(uint256 _wstETHAmount) external view returns (uint256);
+    function getStETHByWstETH(
+        uint256 _wstETHAmount
+    ) external view returns (uint256);
 }
 
-contract UWLidoStrategy is IUWAssetsReport, IUWDebtReport, IUWDeposit, UWBaseStrategy {
+contract UWLidoStrategy is
+    IUWAssetsReport,
+    IUWDebtReport,
+    IUWDeposit,
+    UWBaseStrategy
+{
     /// @notice the LIDO proxy
     ILido internal immutable LIDO;
 
     /// @notice the wrapped stETH token.
-    IwstETH internal immutable wstETH;
+    IwstETH internal immutable WSTETH;
 
     /// @notice the refferal that gets used when submitting ether to LIDO.
     address internal immutable referral;
 
     constructor(ILido _lido, IwstETH _wstETH, address _referral) {
         LIDO = _lido;
-        wstETH = _wstETH;
+        WSTETH = _wstETH;
         referral = _referral;
     }
 
@@ -41,21 +50,35 @@ contract UWLidoStrategy is IUWAssetsReport, IUWDebtReport, IUWDeposit, UWBaseStr
     /// @param position choose which asset to receive stETH or wstETH.
     /// @param asset UWConstants.NATIVE_ASSET is the only asset supported.
     /// @param amount the amount of ether to deposit.
-    function deposit(bytes32 position, address asset, uint256 amount) external payable override {
+    function deposit(
+        bytes32 position,
+        address asset,
+        uint256 amount
+    ) external payable override {
         // Only native Ether is supported.
-        if (asset != UWConstants.NATIVE_ASSET) revert IUWErrors.UNSUPPORTED_ASSET(asset);
+        if (asset != UWConstants.NATIVE_ASSET)
+            revert IUWErrors.UNSUPPORTED_ASSET(asset);
         // Check that the position is one of the two available options.
         if (
-            address(uint160(uint256(position))) != address(LIDO)
-                && address(uint160(uint256(position))) != address(wstETH)
+            address(uint160(uint256(position))) != address(LIDO) &&
+            address(uint160(uint256(position))) != address(WSTETH)
         ) {
             revert IUWErrors.INVALID_POSITION(position);
         }
 
         // Check that the amount to deposit is within the allowed bound of Lido and that the user has enough balance
-        if (amount < 100 wei || amount > address(this).balance || amount > 1000 ether) {
+        if (
+            amount < 100 wei ||
+            amount > address(this).balance ||
+            amount > 1000 ether
+        ) {
             revert IUWErrors.ASSET_AMOUNT_OUT_OF_BOUNDS(
-                asset, 100 wei, address(this).balance < 1000 ether ? address(this).balance : 1000 ether, amount
+                asset,
+                100 wei,
+                address(this).balance < 1000 ether
+                    ? address(this).balance
+                    : 1000 ether,
+                amount
             );
         }
 
@@ -63,9 +86,9 @@ contract UWLidoStrategy is IUWAssetsReport, IUWDebtReport, IUWDeposit, UWBaseStr
         uint256 _receivedAmount = LIDO.submit{value: amount}(referral);
 
         // If the position was for wstETH, then we perform the stETH -> wstETH
-        if (address(uint160(uint256(position))) == address(wstETH)) {
-            LIDO.approve(address(wstETH), _receivedAmount);
-            wstETH.wrap(_receivedAmount);
+        if (address(uint160(uint256(position))) == address(WSTETH)) {
+            LIDO.approve(address(WSTETH), _receivedAmount);
+            WSTETH.wrap(_receivedAmount);
         }
     }
 
@@ -80,8 +103,10 @@ contract UWLidoStrategy is IUWAssetsReport, IUWDebtReport, IUWDeposit, UWBaseStr
         if (address(uint160(uint256(position))) == address(LIDO)) {
             _amount = LIDO.balanceOf(address(this));
             // If this is the wstETH position.
-        } else if (address(uint160(uint256(position))) == address(wstETH)) {
-            _amount = LIDO.getPooledEthByShares(wstETH.getStETHByWstETH(wstETH.balanceOf(address(this))));
+        } else if (address(uint160(uint256(position))) == address(WSTETH)) {
+            _amount = LIDO.getPooledEthByShares(
+                WSTETH.getStETHByWstETH(WSTETH.balanceOf(address(this)))
+            );
         } else {
             revert IUWErrors.INVALID_POSITION(position);
         }
@@ -99,18 +124,26 @@ contract UWLidoStrategy is IUWAssetsReport, IUWDebtReport, IUWDeposit, UWBaseStr
         address _position = address(uint160(uint256(position)));
 
         // Check that the position is one of the two available options.
-        if (_position  != address(LIDO) && _position != address(wstETH)) {
+        if (_position != address(LIDO) && _position != address(WSTETH)) {
             revert IUWErrors.INVALID_POSITION(position);
         }
 
         Asset[] memory _assets = new Asset[](1);
-        _assets[0] = Asset({asset: _position, amount: IERC20(_position).balanceOf(address(this))});
+        _assets[0] = Asset({
+            asset: _position,
+            amount: IERC20(_position).balanceOf(address(this))
+        });
         return _assets;
     }
 
     /// @dev See {IERC165-supportsInterface}.
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IUWDeposit).interfaceId || interfaceId == type(IUWAssetsReport).interfaceId
-            || interfaceId == type(IUWDebtReport).interfaceId || UWBaseStrategy.supportsInterface(interfaceId);
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override returns (bool) {
+        return
+            interfaceId == type(IUWDeposit).interfaceId ||
+            interfaceId == type(IUWAssetsReport).interfaceId ||
+            interfaceId == type(IUWDebtReport).interfaceId ||
+            UWBaseStrategy.supportsInterface(interfaceId);
     }
 }
