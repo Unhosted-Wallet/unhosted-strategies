306d4b28855e01d60bc8f0b68b1d22bea8af9793
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-05-24 22:39:15 +0200

    refactor: added first base for new refactored `UWAaveV2Strategy`

---
+++ src/UWAaveV2Strategy.sol
@@ -0,0 +1,293 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.20;
+
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {WrappedETH} from "./interfaces/external/common/WrappedETH.sol";
+import {ILendingPoolV2} from "./interfaces/external/aave/ILendingPoolV2.sol";
+import {ILendingPoolV2AddressesProvider} from "./interfaces/external/aave/ILendingPoolV2AddressesProvider.sol";
+
+import {IUWDeposit} from "./interfaces/IUWDeposit.sol";
+import {IUWDepositBeneficiary} from "./interfaces/IUWDepositBeneficiary.sol";
+import {IUWWithdraw} from "./interfaces/IUWWithdraw.sol";
+import {IUWBorrow} from "./interfaces/IUWBorrow.sol";
+import {IUWRepay} from "./interfaces/IUWRepay.sol";
+import {IUWDebtReport} from "./interfaces/IUWDebtReport.sol";
+import {IUWDebtHealthReport} from "./interfaces/IUWDebtHealthReport.sol";
+import {IUWAssetsReport, Asset} from "./interfaces/IUWAssetsReport.sol";
+import {UWBaseStrategy} from "./abstract/UWBaseStrategy.sol";
+import {UWConstants} from "./libraries/UWConstants.sol";
+import {IUWErrors} from "./interfaces/IUWErrors.sol";
+
+contract UWCompoundV3Strategy is
+    IUWDeposit,
+    IUWDepositBeneficiary,
+    IUWWithdraw,
+    IUWBorrow,
+    IUWRepay,
+    IUWAssetsReport,
+    IUWDebtReport,
+    IUWDebtHealthReport,
+    UWBaseStrategy
+{
+    enum InterestRateMode {
+        STABLE,
+        VARIABLE
+    }
+
+    // ╭─ Immutable Properties ───────────────────────────────────────────╮
+    /// @notice provided the AaveV2 addresses to use
+    ILendingPoolV2AddressesProvider internal immutable ADDRESSES;
+
+    /// @notice referral code that gets used for actions where we can pass one.
+    uint16 internal immutable REFERRAL;
+
+    /// @notice the implementation of wrapped ether to use.
+    WrappedETH internal immutable WETH;
+    // ╰─ Immutable Properties ───────────────────────────────────────────╯
+
+    constructor(ILendingPoolV2AddressesProvider _addresses, WrappedETH _weth) {
+        ADDRESSES = _addresses;
+        WETH = _weth;
+
+        // TODO: check if we can get a code.
+        REFERRAL = uint16(0);
+    }
+
+    receive() external payable {}
+
+    // ╭─ External & Public Functions ────────────────────────────────────╮
+    /// @notice Deposit an asset into a compound comet.
+    /// @param position the comet to use.
+    /// @param asset the asset to deposit.
+    /// @param amount the amount of the asset to deposit.
+    function deposit(
+        bytes32 position,
+        address asset,
+        uint256 amount
+    ) external payable override {
+        _depositTo(asset, amount, address(this));
+    }
+
+    /// @notice Deposit an asset into a compound comet.
+    /// @param position the comet to use.
+    /// @param asset the asset to deposit.
+    /// @param amount the amount of the asset to deposit.
+    /// @param beneficiary the address to perform the deposit for.
+    function depositTo(
+        bytes32 position,
+        address asset,
+        uint256 amount,
+        address beneficiary
+    ) external payable {
+        _depositTo(asset, amount, address(beneficiary));
+    }
+
+    /// @notice Withdraw an asset from a compound comet.
+    /// @param position the comet to use.
+    /// @param asset the asset to withdraw.
+    /// @param amount the amount of the asset to withdraw.
+    function withdraw(
+        bytes32 position,
+        address asset,
+        uint256 amount
+    ) external override {
+        _withdrawTo(asset, amount, address(this));
+    }
+
+    /// @notice Withdraw an asset from a compound comet.
+    /// @param position the comet to use.
+    /// @param asset the asset to withdraw.
+    /// @param amount the amount of the asset to withdraw.
+    /// @param beneficiary the recipient of the withdrawn assets.
+    function withdrawTo(
+        bytes32 position,
+        address asset,
+        uint256 amount,
+        address beneficiary
+    ) external override {
+        _withdrawTo(asset, amount, beneficiary);
+    }
+
+    /// @notice Repay the base token for a compound comet.
+    /// @param position the comet to use.
+    /// @param asset the base asset to repay.
+    /// @param amount the amount of the asset to repay.
+    function repay(bytes32 position, address asset, uint256 amount) external {
+        InterestRateMode _mode = InterestRateMode(uint8(uint256(position)));
+        _repayTo(_mode, asset, amount, address(this));
+    }
+
+    /// @notice Borrow an asset from a compound comet.
+    /// @param position the comet to use.
+    /// @param asset the base asset to borrow.
+    /// @param amount the amount of the asset to borrow.
+    function borrow(
+        bytes32 position,
+        address asset,
+        uint256 amount
+    ) external override {}
+
+    /// @notice Borrow an asset from a compound comet.
+    /// @param position the comet to use.
+    /// @param asset the base asset to borrow.
+    /// @param amount the amount of the asset to borrow.
+    /// @param beneficiary the recipient of the borrowed assets.
+    function borrowTo(
+        bytes32 position,
+        address asset,
+        uint256 amount,
+        address beneficiary
+    ) public override {}
+    // ╰─ External & Public Functions ────────────────────────────────────╯
+
+    // ╭─ View Functions ─────────────────────────────────────────────────╮
+    /// @notice Reports on the amount of the users assets that are in this strategy.
+    /// @param position the position to check.
+    /// @return _assets of the position.
+    function assets(
+        bytes32 position
+    ) external view returns (Asset[] memory _assets) {}
+
+    /// @notice Reports on the amount debt the user has to this strategy.
+    /// @param position the position to check.
+    /// @return _assets assets of the position.
+    function debt(
+        bytes32 position
+    ) external view returns (Asset[] memory _assets) {}
+
+    /// @notice Reports the health of a position.
+    /// @param position the position to check.
+    /// @return current an amount that represents the current debt.
+    /// @return max an amount at (or above) its no longer possible to take out additional debt against this position.
+    /// @return liquidatable an amount at which the position is at risk of being liquidated.
+    function debtHealth(
+        bytes32 position
+    ) external view returns (uint256, uint256, uint256) {}
+
+    /// @dev See {IERC165-supportsInterface}.
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override returns (bool) {
+        return
+            interfaceId == type(IUWDeposit).interfaceId ||
+            interfaceId == type(IUWDepositBeneficiary).interfaceId ||
+            interfaceId == type(IUWWithdraw).interfaceId ||
+            interfaceId == type(IUWBorrow).interfaceId ||
+            interfaceId == type(IUWRepay).interfaceId ||
+            interfaceId == type(IUWAssetsReport).interfaceId ||
+            interfaceId == type(IUWDebtReport).interfaceId ||
+            interfaceId == type(IUWDebtHealthReport).interfaceId ||
+            UWBaseStrategy.supportsInterface(interfaceId);
+    }
+    // ╰─ View Functions ─────────────────────────────────────────────────╯
+
+    // ╭─ Internal Functions ─────────────────────────────────────────────╮
+    function _depositTo(
+        address _asset,
+        uint256 _amount,
+        address _beneficiary
+    ) internal {
+        ILendingPoolV2 _pool;
+        (_pool, _asset) = _preSendFlow(_asset, _amount);
+
+        _pool.deposit(_asset, _amount, _beneficiary, REFERRAL);
+    }
+
+    function _repayTo(
+        InterestRateMode _mode,
+        address _asset,
+        uint256 _amount,
+        address _beneficiary
+    ) internal {
+        ILendingPoolV2 _pool;
+        (_pool, _asset) = _preSendFlow(_asset, _amount);
+
+        _pool.repay(_asset, _amount, uint256(_mode), _beneficiary);
+    }
+
+    function _borrowTo(
+        InterestRateMode _mode,
+        address _asset,
+        uint256 _amount,
+        address _beneficiary
+    ) internal {
+        // Get the pool address from the registry.
+        ILendingPoolV2 _pool = ILendingPoolV2(ADDRESSES.getLendingPool());
+
+        // If the asset we are withdrawing is an ERC20 we can exit early.
+        if (_asset != UWConstants.NATIVE_ASSET)
+            return
+                _pool.borrow(
+                    _asset,
+                    _amount,
+                    uint256(_mode),
+                    REFERRAL,
+                    _beneficiary
+                );
+
+        // Withdraw WETH to this address.
+        _pool.borrow(
+            address(WETH),
+            _amount,
+            uint256(_mode),
+            REFERRAL,
+            address(this)
+        );
+
+        // Unwrap the WETH.
+        WETH.withdraw(_amount);
+
+        // Send it to the beneficiary if we are not the beneficiary.
+        if (_beneficiary != address(this))
+            SafeTransferLib.safeTransferETH(_beneficiary, _amount);
+    }
+
+    function _withdrawTo(
+        address _asset,
+        uint256 _amount,
+        address _beneficiary
+    ) internal {
+        // Get the pool address from the registry.
+        ILendingPoolV2 _pool = ILendingPoolV2(ADDRESSES.getLendingPool());
+
+        // If the asset we are withdrawing is an ERC20 we can exit early.
+        if (_asset != UWConstants.NATIVE_ASSET) {
+            _pool.withdraw(_asset, _amount, _beneficiary);
+            return;
+        }
+
+        // Withdraw WETH to this address.
+        _pool.withdraw(address(WETH), _amount, address(this));
+
+        // Unwrap the WETH.
+        WETH.withdraw(_amount);
+
+        // Send it to the beneficiary if we are not the beneficiary.
+        if (_beneficiary != address(this))
+            SafeTransferLib.safeTransferETH(_beneficiary, _amount);
+    }
+
+    function _preSendFlow(
+        address _asset,
+        uint256 _amount
+    ) internal returns (ILendingPoolV2, address) {
+        // Handle the native asset and normalize it.
+        if (_asset == UWConstants.NATIVE_ASSET) {
+            // Wrap ETH
+            WETH.deposit{value: _amount}();
+            // Continue as normal but set the token to be used to WETH.
+            _asset = address(WETH);
+        }
+
+        // Get the pool address from the registry.
+        ILendingPoolV2 _pool = ILendingPoolV2(ADDRESSES.getLendingPool());
+
+        // Approve the asset to be deposited.
+        SafeTransferLib.safeApproveWithRetry(_asset, address(_pool), _amount);
+
+        return (_pool, _asset);
+    }
+    // ╰─ Internal Functions ─────────────────────────────────────────────╯
+}
